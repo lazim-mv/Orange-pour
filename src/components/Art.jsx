7420ff5a63e62022938f9d59ef337e3f97fbcd1d
lazim-mv <lazimlatheef@gmail.com>
2025-09-21 17:02:01 +0530

    footer

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -30,12 +30,12 @@ const Art = () => {
                 scale: 1.3,
                 maskPosition: 'center',
                 maskSize: '400%',
-                duration: 1,
+                duration: 2,
                 ease: "power1.inOut",
             })
             .to('#masked-content', {
                 opacity: 1,
-                duration: 1,
+                duration: 2,
                 ease: "power1.inOut",
             })
 
